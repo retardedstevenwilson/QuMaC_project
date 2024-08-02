1b640c44ae97e3e71239c25820d7290226ae6474
retardedstevenwilson <ayush.nema@tifr.res.in>
2024-08-02 15:31:11 +0530

    Arduino problem solved. created functions for valves, initial N2 burst, and timer circuit

--- hardconnections.py
+++ hardconnections.py
@@ -5,15 +5,25 @@ import time
 #set all timescales in seconds
 arduino=Q.ArduinoConnector('COM6')
 
-
-O2_relay=4
-N2_relay=3
-buffer_to_main_valve_relay=2
 roughing_relay=1
+buffer_to_loadlock_relay=2
+N2_relay=3
+O2_relay=4
+loadlock_to_main_relay=5
 
 buffervolume=Q.pgauge(name='buffervolume',port='COM5')
 mainchamber=Q.pgauge(name='mainchamber',port='COM7')
 loadlock=Q.pgauge(name='loadlock',port='COM4')
+
+
+def loadlock_to_main_valve(value):
+    arduino.toggle_relay(loadlock_to_main_relay,state=value)
+
+def roughing_valve(value):
+    arduino.toggle_relay(roughing_relay,state=value)
+
+def timetoggle_buffer_to_loadlock_valve(toggletime):
+    arduino.timetoggle_relay(buffer_to_loadlock_relay,toggletime=toggletime)
 
 
 def O2_buffer_toggle(p_opt,duration=10,toggletime=0.1):
@@ -41,70 +51,22 @@ def O2_buffer_toggle(p_opt,duration=10,toggletime=0.1):
         print("Final buffer presure = {}. Toggling stopped after {} counts".format(p_current,count))
 
 
-
-# def mainchamber_toggle(p_opt,duration=5,toggletime=0.1):
-#     '''inputs: p_opt, duration of logging after each toggle, and toggletime'''
-#     mainchamber.log_serial_data(timeout=duration)
-#     p_current= mainchamber.read_last_entry()
-#     thr=0.01
-#     p_max=760
-#     count=0
-#     if p_current >=p_max:
-#         print('ALERT: MAINCHAMBER PRESSURE REACHED MAXIMUM')
-#     else:    
-#         while p_opt-p_current>=thr:
-#             arduino.timetoggle_relay(buffervolume.relay_no,toggletime)
-#             time.sleep(1)
-#             mainchamber.log_serial_data(timeout=duration)
-#             p_current= mainchamber.read_last_entry()
-#             count+=1
-#             print("Toggle {count} copleted.")
-#         print("Final mainchamber presure = {p_current}. Toggling stopped after {counts} counts")
-
-
-
-# def loadlock_toggle(p_opt,duration=5,toggletime=0.1):
-#     '''inputs: p_opt, duration of logging after each toggle, and toggletime'''
-#     loadlock.log_serial_data(timeout=duration)
-#     p_current= loadlock.read_last_entry()
-#     thr=0.1
-#     p_max=760
-#     count=0    
-#     if p_current >=p_max:
-#         print('ALERT: LOADLOCK PRESSURE REACHED MAXIMUM')
-#     else:
-#         while p_opt-p_current >=thr:
-#             print("p_current = {p_current}, togglecount = {count} \n")
-#             arduino.timetoggle_relay(loadlock.relay_no,toggletime)
-#             time.sleep(1)
-#             loadlock.log_serial_data(timeout=duration)
-#             p_current= loadlock.read_last_entry()
-#             count+=1
-#             print("Toggle {count} copleted.")
-#         print("Final loadlock presure = {p_current}. Toggling stopped after {counts} counts")
-
-
-def open_buffer(duration):
-    arduino.timetoggle_relay(buffer_to_main_valve_relay,duration=duration)
-
-
             
-def N2_toggle(p_opt,duration=10,toggletime=0.1):
-
-    mainchamber.log_serial_data(timeout=5)
-    p_current= mainchamber.read_last_entry()
+def N2_toggle(p_opt,duration=5,toggletime=0.1,initial_toggle=1.5):
+    '''N2 Charging in the loadlock'''
+    arduino.timetoggle_relay(N2_relay,toggletime=initial_toggle)
+    loadlock.log_serial_data(timeout=5)
+    p_current= loadlock.read_last_entry()
+    
     thr=0.1
-    # p_max=760
-    # if p_current >=p_max:
-    #     print('ALERT: MAINCHAMBER PRESSURE REACHED MAXIMUM')
-    # else:
-    count=0    
+    count=0
+
     try:
         while p_opt-p_current>=thr:
             arduino.timetoggle_relay(N2_relay,toggletime)
             time.sleep(1)
-            mainchamber.log_serial_data(timeout=duration)
-            p_current= mainchamber.read_last_entry()
+            loadlock.log_serial_data(timeout=duration)
+            p_current= loadlock.read_last_entry()
             print("current p = ",p_current)
             count+=1
             print("Toggle {} completed. Waiting 5 seconds for stabilization".format(count))
@@ -115,17 +77,18 @@ def N2_toggle(p_opt,duration=10,toggletime=0.1):
 
 
 def roughing_toggle(p_opt,duration=5,toggletime=10):
+    '''Starts roughing process in the loadlock'''
     thr=0.1
     try:
-        mainchamber.log_serial_data(timeout=5)
-        p_current= mainchamber.read_last_entry()
+        loadlock.log_serial_data(timeout=5)
+        p_current= loadlock.read_last_entry()
         
         arduino.toggle_relay(roughing_relay,True)
         
         while p_current - p_opt >= thr:
-            mainchamber.log_serial_data(timeout=duration)
+            loadlock.log_serial_data(timeout=duration)
             time.sleep(1)
-            p_current= mainchamber.read_last_entry()
+            p_current= loadlock.read_last_entry()
             print("current p = {}. Roughing continues... ".format(p_current))
             time.sleep(1)
         
