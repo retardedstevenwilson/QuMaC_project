6d3d9d51716e2d4eb3777c427b3e44e0ea593665
retardedstevenwilson <ayush.nema@tifr.res.in>
2024-09-04 13:36:10 +0530

    merging hardconnection part to toolkit to avoid multiple scripts

--- console_2_toolkit.py
+++ console_2_toolkit.py
@@ -93,7 +93,7 @@ class pgauge_console:
     def __init__(self,name,port):
         self.port = port
         self.baudrate =9600
-        self.pressurevalue =0
+        self.pressurevalue = 1000
         self.name = name
         self.log_file = f"{self.name}_log.txt"
         print(f"Connection: {self.name} on {self.port}")
@@ -155,7 +155,83 @@ class pgauge_console:
                 textbox.insert(tk.END, line)
 
             
+arduino=ArduinoConnector_console('COM6')
+
+roughing_relay=1
+buffer_to_loadlock_relay=2
+N2_relay=3
+O2_relay=4
+loadlock_to_main_relay=5
+
+buffervolume=pgauge_console(name='buffervolume',port='COM3') #change comport to 5 for madhavi's laptop
+mainchamber=pgauge_console(name='mainchamber',port='COM7')
+loadlock=pgauge_console(name='loadlock',port='COM4')
+
+
+def loadlock_to_main_valve(value):
+    arduino.toggle_relay(loadlock_to_main_relay,state=value)
+
+def roughing_valve(value):
+    arduino.toggle_relay(roughing_relay,state=value)
+
+
+def timetoggle_buffer_to_loadlock_valve(toggletime):
+    arduino.timetoggle_relay(buffer_to_loadlock_relay,toggletime=toggletime)
+
+
+
+            
+def N2_toggle(p_opt,duration=3,toggletime=0.2,initial_toggle=1.5):
+    '''N2 Charging in the loadlock'''
+    print("Starting N2 toggle")
+    arduino.timetoggle_relay(N2_relay,toggletime=initial_toggle)
+    loadlock.log_serial_data(timeout=5)
+    p_current= loadlock.read_last_entry()
     
+    thr=0.1*p_opt
+    count=0
+
+    try:
+        while p_opt-p_current>=thr:
+            arduino.timetoggle_relay(N2_relay,toggletime)
+            # time.sleep(1)
+            loadlock.log_serial_data(timeout=duration)
+            p_current= loadlock.read_last_entry()
+            print("current p = ",p_current)
+            count+=1
+            print("Toggle {} completed.".format(count))
+            # time.sleep(5)
+            print("Final mainchamber presure = {}. Toggling stopped after {} counts".format(p_current,count))
+    except KeyboardInterrupt:
+        print("Toggle process manually stopped")
+
+
+def roughing_toggle(p_opt,duration=5):
+    '''Starts roughing process in the loadlock'''
+    thr=0.1*p_opt
+    print("Starting roughing toggle. Opening the valve")
+    roughing_valve(True)
+
+    try:
+        loadlock.log_serial_data(timeout=5)
+        p_current= loadlock.read_last_entry()
+        
+        arduino.toggle_relay(roughing_relay,True)
+        
+        while p_current - p_opt >= thr:
+            loadlock.log_serial_data(timeout=duration)            
+            p_current= loadlock.read_last_entry()
+            print("current p = {}. Roughing continues... ".format(p_current))
+            time.sleep(1)
+        
+        print("Optimum P reached. Turning off the roughing relay")
+        arduino.toggle_relay(roughing_relay,False)
+    
+    except KeyboardInterrupt:
+        print("Roughing toggle manually stopped")
+        arduino.toggle_relay(roughing_relay,False)
+        
+    print("Final mainchamber presure = {}".format(p_current))
 
     
 
